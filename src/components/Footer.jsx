edc8ab49b3c67f82a8f26d8aa6b48a426a0a1b53
neducod <nedunony16@gmail.com>
2026-08-12 14:38:00 +0100

    uncommented lucide icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-// import { Eye, Instagram, Facebook, Twitter, ArrowRight, MapPin, Phone, Mail } from 'lucide-react'
+import { Eye, Instagram, Facebook, Twitter, ArrowRight, MapPin, Phone, Mail } from 'lucide-react'
 import { useState } from 'react'
 
 const hours = [
