3b97da837a5c61e717540de8a3f1ccf00255cb93
neducod <nedunony16@gmail.com>
2026-08-14 03:36:15 +0100

    turns out that the testimonial slider has already been declared

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import {
 import { Reveal, PageShell, stagger, staggerItem } from '../components/Motion.jsx'
 import ApertureMark from '../components/ApertureMark.jsx'
 import ImagePlaceholder from '../components/ImagePlaceholder.jsx'
-import TestimonialSlider from '../components/TestimonialSlider.jsx'
+// import TestimonialSlider from '../components/TestimonialSlider.jsx'
 
 const stats = [
   { value: '15+', label: 'Years of Clinical Experience' },
